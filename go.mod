2342b494693e087b65719bc8991bbaa35176b106
이민영 <leo@leo-m2.local>
2023-09-20 18:21:04 +0900

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/9oogle79/myfirstpkg
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,15 @@
+package myfirstpkg
+
+import "fmt"
+
+//func myFirstFunc() {
+//	fmt.Println("Not see!!!")
+//}
+
+//func MyFirstFunc() {
+//	fmt.Println("My first package!!!")
+//}
+
+func t(){
+	fmt.Println("tttt")
+}
